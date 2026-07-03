da5ce063c376ab5683cb44c13a7077c6c4c2a179
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-03 11:36:44 +0530

    Add variables program in Java

---
+++ 01_Basics/Variables.java
@@ -0,0 +1,26 @@
+class Variables
+{
+    public static void main(String A[])
+    {
+        // Variable Declaration
+
+        char grade;
+        int age;
+        float marks;
+        double salary;
+
+        // Variable Initialization
+
+        grade = 'A';
+        age = 25;
+        marks = 99.89f;
+        salary = 78.67890;
+
+        // Print Values
+
+        System.out.println("Grade : " + grade);
+        System.out.println("Age : " + age);
+        System.out.println("Marks : " + marks);
+        System.out.println("Salary : " + salary);
+    }
+}
