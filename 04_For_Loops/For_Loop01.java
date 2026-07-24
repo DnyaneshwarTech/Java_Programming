01945301b653c126f874945a384331c42e5a023b
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-25 00:03:48 +0530

    Add Java program for for loop

---
+++ 04_For_Loops/For_Loop01.java
@@ -0,0 +1,12 @@
+class For_Loop01
+{
+    public static void main(String A[])
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= 5; iCnt++)
+        {
+            System.out.println(iCnt);
+        }
+    }
+}
