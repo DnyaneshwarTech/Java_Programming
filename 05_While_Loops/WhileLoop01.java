a5e5bf298078ee3d1aa5de2d9b3a043ee36d5a05
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-22 23:45:10 +0530

    Add Java while loop program

---
+++ 05_While_Loops/WhileLoop01.java
@@ -0,0 +1,13 @@
+class WhileLoop01
+{
+    public static void main(String A[])
+    {
+        int iCnt = 1;
+
+        while(iCnt <= 5)
+        {
+            System.out.println(iCnt);
+            iCnt++;
+        }
+    }
+}
