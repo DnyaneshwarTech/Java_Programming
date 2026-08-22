4221d71fb3ba274d5c9a66e4095768b234ea6fc7
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-22 23:58:25 +0530

    Add Java  program for even numbers

---
+++ 05_While_Loops/EvenNumbers_03.java
@@ -0,0 +1,13 @@
+class EvenNumbers_03 
+{
+    public static void main(String A[])
+    {
+        int iCnt = 2;
+
+        while(iCnt <= 20)
+        {
+            System.out.println(iCnt);
+            iCnt = iCnt + 2;
+        }
+    }
+}
