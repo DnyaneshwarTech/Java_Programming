f7c8f5af828510fc99e443dceb1ea33dcd002e8e
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-22 23:55:23 +0530

    Add Java program print 10 to 1

---
+++ 05_While_Loops/PrintTentoOne_03.java
@@ -0,0 +1,13 @@
+class PrintTentoOne_03
+{
+    public static void main(String A[])
+    {
+        int iCnt = 10;
+
+        while(iCnt >= 1)
+        {
+            System.out.println(iCnt);
+            iCnt--;
+        }
+    }
+}
