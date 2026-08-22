e7b5dbaeede9e6c988776763af990ba5c183f2fd
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-22 23:51:43 +0530

    Add Java program to print 1 to 10

---
+++ 05_While_Loops/PrintOnetoTen_02.java
@@ -0,0 +1,13 @@
+class PrintOnetoTen_02 
+{
+    public static void main(String A[])
+    {
+        int iCnt = 1;
+
+        while(iCnt <= 10)
+        {
+            System.out.println(iCnt);
+            iCnt++;
+        }
+    }
+}
