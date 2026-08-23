61d8baefa64051943384d5a7d9f52465159b2221
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-23 23:57:13 +0530

    Add table and sum programs using while loop

---
+++ 05_While_Loops/Table06.java
@@ -0,0 +1,14 @@
+class Table06
+{
+    public static void main(String A[])
+    {
+        int iNum = 5;
+        int iCnt = 1;
+
+        while(iCnt <= 10)
+        {
+            System.out.println(iNum * iCnt);
+            iCnt++;
+        }
+    }
+}
